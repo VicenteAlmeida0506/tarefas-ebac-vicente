b03d33bc15280977d5bb83d01aca39cd4b3bd7b3
VicenteAlmeida0506 <vicente.almeidaluiz@gmail.com>
2024-03-21 21:21:03 -0300

    Add lista

--- src/ExercicioModulo/CarrosDaMarca.java
+++ src/ExercicioModulo/CarrosDaMarca.java
@@ -1,19 +1,25 @@
+
 package ExercicioModulo;
 
-public class CarrosDaMarca{
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrosDaMarca {
     public static void main(String[] args) {
-        Renault<String,Integer,Double,String> duster = new Renault("Duster",2021,1.6,"SUV");
-        Renault<String,String,Double,String> oroch = new Renault<>("Oroch","0 KM",2.0,"Picape");
-        Renault<String,Integer,String,String> kwid = new Renault<>("Kwid",2022,"1.Fraco","Popular");
+        List<Renault<?, ?, ?, ?>> listaCarros = new ArrayList<>();
 
-        duster.imprimirCaracterisitcas();
-        duster.separador();
-        oroch.imprimirCaracterisitcas();
-        oroch.separador();
-        kwid.imprimirCaracterisitcas();
-        kwid.separador();
-        kwid.mensagemFinal();
+        Renault<String, Integer, Double, String> duster = new Renault<>("Duster", 2021, 1.6, "SUV");
+        Renault<String, String, Double, String> oroch = new Renault<>("Oroch", "0 KM", 2.0, "Picape");
+        Renault<String, Integer, String, String> kwid = new Renault<>("Kwid", 2022, "1.Fraco", "Popular");
 
+        listaCarros.add(duster);
+        listaCarros.add(oroch);
+        listaCarros.add(kwid);
 
+        for (Renault<?, ?, ?, ?> carro : listaCarros) {
+            carro.imprimirCaracterisitcas();
+            carro.separador();
         }
+        kwid.mensagemFinal();
+    }
 }
