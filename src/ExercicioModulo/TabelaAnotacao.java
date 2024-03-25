5c32250218ee46d3dc5f2902205c57108bad4e01
VicenteAlmeida0506 <vicente.almeidaluiz@gmail.com>
2024-03-24 22:24:26 -0300

    Commit Exercicio

--- src/ExercicioModulo/TabelaAnotacao.java
+++ src/ExercicioModulo/TabelaAnotacao.java
@@ -1,6 +1,21 @@
 package ExercicioModulo;
+
+import java.lang.annotation.Annotation;
+
 @Tabela(nome = "Tabela de Anotação",linhasEColunas = 10,departamento = "RH")
 public class TabelaAnotacao {
+    public static void main(String[] args) {
+
+        Class<?> classe = TabelaAnotacao.class;
+        Annotation anotacao = classe.getAnnotation(Tabela.class);
+        if (anotacao instanceof Tabela) {
+            Tabela tabela = (Tabela) anotacao;
+            System.out.println("Nome: " + tabela.nome());
+            System.out.println("Departamento: " + tabela.departamento());
+            System.out.println("Linhas e Colunas: " + tabela.linhasEColunas());
+
+        }
 
     }
+}
 
